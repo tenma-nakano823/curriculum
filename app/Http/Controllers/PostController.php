f47407edfb2b29b5a9b816f2445d0e2187266c5a
tenma-nakano823 <t.nakano.806@ms.saitama-u.ac.jp>
2024-05-06 02:35:47 +0900

    ブログ投稿一覧の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,8 +17,11 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        //return $post->get();//$postの中身を戻り値にする。
-        return view('posts.index') -> with(['posts' => $post->get()]);
+        //return $post->get(); $postの中身を戻り値にする。
+        //return view('posts.index') -> with(['posts' => $post->get()]);  全件取得
+        //return view('posts.index')->with(['posts' => $post->getByLimit()]);  //件数取得
         //blade内で使う変数'posts'と設定. 'posts'の中身にgetを使い, インスタンス化した$postを代入
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);  //件数取得
+        //getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
 }
