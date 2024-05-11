65caae36ab9f1432054da44ed9b2d1e5cdba016c
tenma-nakano823 <t.nakano.806@ms.saitama-u.ac.jp>
2024-05-11 17:13:18 +0900

    ブログ投稿削除

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
@@ -12,6 +13,8 @@ class Post extends Model
         'body',
     ];
     use HasFactory;
+    use SoftDeletes;
+    
     
     /*
     public function getByLimit(int $limit_count = 3)
