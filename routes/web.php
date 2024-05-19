1d01b774e9a417448ce2acf7e4b2f1107c676a3e
tenma-nakano823 <t.nakano.806@ms.saitama-u.ac.jp>
2024-05-19 17:15:14 +0900

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,23 +14,18 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
-Route::get('/', [PostController::class, 'index']);  
-Route::get('/categories/{category}', [CategoryController::class,'index']);
-/*
-Route::get('/',function () {
-    return view('posts.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-*/
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-//'/posts/{対象データのID}'にGetリクエストが来たら, PostControllerメソッドのshowメソッドを実行する.
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{post}/edit',[PostController::class, 'edit']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'delete']);
+
+require __DIR__.'/auth.php';
